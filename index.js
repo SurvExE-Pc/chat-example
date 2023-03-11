0b5c7099bdcdcf9fe71657386e87fca12f3a8112
Surv <83309545+SurvExE-Pc@users.noreply.github.com>
2023-03-12 00:07:38 +0600

    WFQWFQWF
    WQFQWFW

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+app.get('/socket', (req, res) => {
+  res.sendFile(__dirname + '/socketTest.html');
+});
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+  socket.on('data', msg => {
+    io.emit('data', msg);
+    console.log(msg);
   });
 });
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+  console.log(`Server running. ${port}`);
 });
